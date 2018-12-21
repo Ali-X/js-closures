aacaee1183e2c588b1b0c5215501d05487adeaf7
Ali-X <alina240895@yandex.ru>
2018-12-21 21:04:04 +0200

    Implement third task

---
+++ js/script3.js
@@ -0,0 +1,11 @@
+function multiA(a) {
+  return function (b) {
+    return a * b;
+  }
+}
+
+let multi3 = multiA(3);
+let multi4 = multiA(4);
+
+console.log("3 * 5 = " + multi3(5));
+console.log("4 * 5 = " + multi4(5));
