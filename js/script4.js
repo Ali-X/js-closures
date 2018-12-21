cf664f25135498d33d2d1c94ddeb0673ddbcdf85
Ali-X <alina240895@yandex.ru>
2018-12-21 21:18:04 +0200

    Implement fourth task

---
+++ js/script4.js
@@ -0,0 +1,23 @@
+function incrementCounter() {
+  let obj = {
+    i: 0,
+    increment: function () {
+      return ++obj.i;
+    }
+  };
+
+  return obj;
+}
+
+let incFunc = incrementCounter();
+
+console.log("First");
+console.log(incFunc.increment());
+console.log(incFunc.increment());
+console.log(incFunc.increment());
+
+incFunc = incrementCounter();
+
+console.log("Second");
+console.log(incFunc.increment());
+console.log(incFunc.increment());
